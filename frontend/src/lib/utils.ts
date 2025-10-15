77d46bfb156bf224d3317c8cb6f6e37b50690d42
Maximillian <maxsilvauk@gmail.com>
2025-10-16 00:48:45 +0200

    Update utils.ts

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -3,6 +3,13 @@ import { twMerge } from 'tailwind-merge';
 import { appToast } from './toast-factory';
 import { coinbaseWallet, walletConnect, injected } from 'wagmi/connectors';
 
+type TMetadata = {
+  name: string;
+  description: string;
+  url: string;
+  icons: string[];
+};
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -18,7 +25,7 @@ export const getWalletConnectProjectId = (): string => {
   return projectId || process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '';
 };
 
-export const createWeb3ModalConnectors = (metadata: any) => {
+export const createWeb3ModalConnectors = (metadata: TMetadata) => {
   const projectId = getWalletConnectProjectId();
   if (!projectId) {
     throw new Error('WalletConnect Project ID is not defined');
