f242ba57003d751f9aea31db901be1eec23ca550
Maximillian <maxsilvauk@gmail.com>
2025-10-16 00:19:38 +0200

    Test

--- frontend/src/components/Providers/Web3ModalProvider/config.ts
+++ frontend/src/components/Providers/Web3ModalProvider/config.ts
@@ -34,10 +34,7 @@ const getConnectors = () => {
 
   return [
     walletConnect({
-      projectId:
-        process.env.WALLETCONNECT_PROJECT_ID ||
-        process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ||
-        '1f7ec19a11311c8e148ab385a70b35ce',
+      projectId: process.env.WALLETCONNECT_PROJECT_ID || 'unknown',
       metadata,
       showQrModal: false,
     }),
