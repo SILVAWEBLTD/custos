a873bfac0b641a4476b5c393485ecc3e74de191e
Maximillian <maxsilvauk@gmail.com>
2025-10-16 13:48:32 +0200

    Update config.ts

--- frontend/src/components/Providers/Web3ModalProvider/config.ts
+++ frontend/src/components/Providers/Web3ModalProvider/config.ts
@@ -17,6 +17,11 @@ const chains = [mainnet, sepolia] as const;
 export const WALLET_CONNECT_PROJECT_ID =
   process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '';
 
+console.log(
+  'process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID',
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
+);
+
 // Only create connectors on the client side
 const getConnectors = () => {
   if (typeof window === 'undefined') {
