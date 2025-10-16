25c50ca232a1237f65dfd3f22f4fdab5bb2039ba
Maximillian <maxsilvauk@gmail.com>
2025-10-16 11:50:19 +0200

    Update index.tsx

--- frontend/src/components/Providers/Web3ModalProvider/index.tsx
+++ frontend/src/components/Providers/Web3ModalProvider/index.tsx
@@ -146,7 +146,7 @@ export function Web3ModalProvider({ children }: { children: ReactNode }) {
     if (!modalCreated) {
       createWeb3Modal({
         wagmiConfig: config,
-        projectId: '1f7ec19a11311c8e148ab385a70b35ce',
+        projectId: process.env.WALLET_CONNECT_PROJECT_ID || '',
         enableAnalytics: true,
         themeMode: 'dark',
         featuredWalletIds: [
