5984ff9acc0aaaf0a78ba4952b02649922f4448e
Maximillian <maxsilvauk@gmail.com>
2025-10-16 11:44:51 +0200

    Update config.ts

--- frontend/src/components/Providers/Web3ModalProvider/config.ts
+++ frontend/src/components/Providers/Web3ModalProvider/config.ts
@@ -20,13 +20,7 @@ const getConnectors = () => {
 
   return [
     walletConnect({
-      projectId:
-        process.env.WALLET_CONNECT_PROJECT_ID ??
-        (() => {
-          throw new Error(
-            'NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID is not defined'
-          );
-        })(),
+      projectId: process.env.WALLET_CONNECT_PROJECT_ID || '',
       metadata,
       showQrModal: true,
     }),
