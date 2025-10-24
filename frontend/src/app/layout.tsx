fde3d93b51622850cff2915de0c412916424753f
Max Silva <maxsilvauk@gmail.com>
2025-10-25 00:22:02 +0200

    Staging (#18)
    * Update protect-production.yml (#16)
    
    * Added always use https flag to terraform + updated databased uuid
    
    * delete-public-svgs
    
    * Placeholder under construction page added

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { Web3ModalProvider } from '@/components/Providers/Web3ModalProvider';
-import { Toaster } from '@/components/ui/sonner';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -15,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Custos',
-  description: 'DApp',
+  title: 'custos.space | DeFi platform',
+  description: 'Custos application',
 };
 
 export default function RootLayout({
@@ -29,8 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Web3ModalProvider>{children}</Web3ModalProvider>
-        <Toaster />
+        {children}
       </body>
     </html>
   );
