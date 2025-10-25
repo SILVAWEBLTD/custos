35f8f34ccd4ed62a186598fb4d4de2dd622877e8
Maximillian <maxsilvauk@gmail.com>
2025-10-25 10:16:33 +0200

    Add FlipWords component to the project

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import { FlipWords } from '@/components/ui/flip-words';
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 py-16 text-center bg-black">
@@ -12,8 +14,12 @@ export default function HomePage() {
           priority
         />
 
-        <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-4xl font-bold text-transparent md:text-4xl text-left max-w-xl mb-4">
-          custos.space
+        <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-4xl font-bold text-transparent md:text-4xl text-left max-w-xl mb-4 flex flex-wrap items-center">
+          <span>custos</span>
+          <FlipWords
+            words={['.space', 'DeFi', 'DEX', 'AI powered']}
+            className="ml-[-4px] text-4xl text-white"
+          />
         </h1>
         <p className="max-w-xl text-md text-muted-foreground text-justify">
           <b className="text-gray-400">Custos which is coming soon</b> is a
